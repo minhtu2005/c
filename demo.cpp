b541c40ff0cf7c251c7211b57a218946938ddbca
minhtu2005 <150407992+minhtu2005@users.noreply.github.com>
2023-12-08 08:28:04 +0700

    first commit

---
+++ demo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by PC on 12/6/2023.
+//
+
+#include "demo.h"
---
+++ demo.h
@@ -0,0 +1,14 @@
+//
+// Created by PC on 12/6/2023.
+//
+
+#ifndef UNTITLED_DEMO_H
+#define UNTITLED_DEMO_H
+
+
+class demo{
+
+};
+
+
+#endif //UNTITLED_DEMO_H
